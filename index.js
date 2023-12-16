8efb442c0df9529fa32867de317f56facadcce84
Patricia Frogoso <pfrogoso@gmail.com>
2023-12-16 12:22:26 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+console.log (customerName);
+
+var customerName = 'bob'
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    console.log(bestCustomer);
+}
+const leastFavoriteCustomer = 'unsuccessfully tries to reassign the least favorite customer'
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'Assignment to constant variable'
+}
